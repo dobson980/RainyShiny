434eee7f103abd3dcd0204dd2ede1a4b9cbbfe92
Tom Dobson <dobson980@gmail.com>
2017-07-09 23:25:37 -0700

    GUI
    Initial GUI, Autloayout, and basic data structure.

---
+++ RainyShiny/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  RainyShiny
+//
+//  Created by Tom Dobson on 7/9/17.
+//  Copyright © 2017 Dobson Studios. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://samples.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "2f1099029c207a254db4b053b691c2e1"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(APP_ID)\(API_KEY)"
+
+
---
+++ RainyShiny/WeatherViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherViewController.swift
+//  RainyShiny
+//
+//  Created by Tom Dobson on 7/9/17.
+//  Copyright © 2017 Dobson Studios. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var currentLocationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var weatherTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
+        
+        print(CURRENT_WEATHER_URL)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+
+}
+
